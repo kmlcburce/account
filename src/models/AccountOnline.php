9b921ba7a43303dcdbf7ec8255d73f3517a434d0
kennCK <kennettecanales@gmail.com>
2020-02-12 09:51:40 +0800

    update

--- src/models/AccountOnline.php
+++ src/models/AccountOnline.php
@@ -5,7 +5,7 @@ namespace Increment\Account\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\APIModel;
-class AccountOnlines extends APIModel
+class AccountOnline extends APIModel
 {
     protected $table = 'account_onlines';
     protected $fillable = ['account_id', 'status'];
