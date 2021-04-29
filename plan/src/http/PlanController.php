cae85a26e0d8a72d29ce28a282218788c6f4163c
= <=>
2021-04-29 23:51:35 +0800

    update

--- plan/src/http/PlanController.php
+++ plan/src/http/PlanController.php
@@ -2,7 +2,7 @@
 
 namespace Increment\Account\Plan\Http;
 
-use Increment\Account\Models\Plan\Plan;
+use Increment\Account\Plan\Models\\Plan;
 use App\Http\Controllers\APIController;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
--- plan/src/http/PlanHistoryController.php
+++ plan/src/http/PlanHistoryController.php
@@ -2,7 +2,7 @@
 
 namespace Increment\Account\Plan\Http;
 
-use Increment\Account\Models\Plan\PlanHistory;
+use Increment\Account\Plan\Models\\PlanHistory;
 use App\Http\Controllers\APIController;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
