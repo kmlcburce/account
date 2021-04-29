abdcdf09d275c492fc0538286c84bfedfcee8bbb
= <=>
2021-04-29 23:53:42 +0800

    update

--- plan/src/http/PlanController.php
+++ plan/src/http/PlanController.php
@@ -2,7 +2,7 @@
 
 namespace Increment\Account\Plan\Http;
 
-use Increment\Account\Plan\Models\\Plan;
+use Increment\Account\Plan\Models\Plan;
 use App\Http\Controllers\APIController;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
