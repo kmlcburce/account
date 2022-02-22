3a6209f608e2acecc4d58efe47d83969b3dadd16
kennck <kennettecanales@gmail.com>
2022-02-22 20:17:31 +0800

    update

--- merchant/src/http/MerchantController.php
+++ merchant/src/http/MerchantController.php
@@ -47,7 +47,7 @@ class MerchantController extends APIController
   public function generateCode()
   {
     $code = 'mer_' . substr(str_shuffle($this->codeSource), 0, 60);
-    $codeExist = Merchant::where('id', '=', $code)->get();
+    $codeExist = Merchant::where('code', '=', $code)->get();
     if (sizeof($codeExist) > 0) {
       $this->generateCode();
     } else {
